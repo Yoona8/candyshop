61cc3e1603b957feb541d480213c60318ab800f2
Nikkie8 <nikkie223shin@gmail.com>
2018-11-05 13:04:28 +0300

    Refactored filters

--- js/filter.js
+++ js/filter.js
@@ -63,6 +63,58 @@
     });
   };
 
+  var filterByTypes = function (filters, goods) {
+    if (filters.length > 0) {
+      return goods.filter(function (good) {
+        var filterKind = filterKindToInputId[good.kind];
+        var isApplied = filters.indexOf(filterKind) > -1;
+        return isApplied;
+      });
+    }
+
+    return goods;
+  };
+
+  var filterByNutritionFacts = function (filters, goods) {
+    if (filters.length > 0) {
+      return goods.filter(function (good) {
+        return filters.indexOf('filter-sugar-free') > -1 ? !good.nutritionFacts.sugar : true;
+      }).filter(function (good) {
+        return filters.indexOf('filter-gluten-free') > -1 ? !good.nutritionFacts.gluten : true;
+      }).filter(function (good) {
+        return filters.indexOf('filter-vegetarian') > -1 ? good.nutritionFacts.vegetarian : true;
+      });
+    }
+
+    return goods;
+  };
+
+  var filterByAvailability = function (filters, goods) {
+    if (filters.indexOf('filter-availability') > -1) {
+      return goods.filter(function (good) {
+        return filters.indexOf('filter-availability') > -1 ? good.amount > 0 : true;
+      });
+    }
+
+    return goods;
+  };
+
+  var filterByFavorite = function (filters, goods) {
+    if (filters.indexOf('filter-favorite') > -1) {
+      return goods.filter(function (good) {
+        return good.favorite;
+      });
+    }
+
+    return goods;
+  };
+
+  var filterByPrice = function (filters, goods) {
+    return goods.filter(function (good) {
+      return good.price >= filters.min && good.price <= filters.max;
+    });
+  };
+
   var sortByPopular = function (goods) {
     return goods;
   };
@@ -92,8 +144,12 @@
     'filter-rating': sortByRating
   };
 
-  var sortGoods = function (filterName, goods) {
-    sortingInputIdToSortingFunctionName[filterName](goods);
+  var sortGoods = function (filters, goods) {
+    if (filters.length > 0) {
+      sortingInputIdToSortingFunctionName[filters[0]](goods);
+    }
+
+    return;
   };
 
   var priceQuantityElement = document.querySelector('.range__count');
@@ -112,43 +168,13 @@
     getFilteredGoods: function (filters, goods) {
       var filteredGoods = goods;
 
-      if (filters.foodTypes.length > 0) {
-        filteredGoods = goods.filter(function (good) {
-          var filterKind = filterKindToInputId[good.kind];
-          var isApplied = filters.foodTypes.indexOf(filterKind) > -1;
-          return isApplied;
-        });
-      }
+      filteredGoods = filterByTypes(filters.foodTypes, filteredGoods);
+      filteredGoods = filterByNutritionFacts(filters.nutritionFacts, filteredGoods);
+      filteredGoods = filterByAvailability(filters.other, filteredGoods);
+      filteredGoods = filterByFavorite(filters.other, filteredGoods);
+      filteredGoods = filterByPrice(filters.prices, filteredGoods);
 
-      if (filters.nutritionFacts.length > 0) {
-        filteredGoods = filteredGoods.filter(function (good) {
-          return filters.nutritionFacts.indexOf('filter-sugar-free') > -1 ? !good.nutritionFacts.sugar : true;
-        }).filter(function (good) {
-          return filters.nutritionFacts.indexOf('filter-gluten-free') > -1 ? !good.nutritionFacts.gluten : true;
-        }).filter(function (good) {
-          return filters.nutritionFacts.indexOf('filter-vegetarian') > -1 ? good.nutritionFacts.vegetarian : true;
-        });
-      }
-
-      if (filters.other.indexOf('filter-availability') > -1) {
-        filteredGoods = filteredGoods.filter(function (good) {
-          return filters.other.indexOf('filter-availability') > -1 ? good.amount > 0 : true;
-        });
-      }
-
-      if (filters.other.indexOf('filter-favorite') > -1) {
-        filteredGoods = filteredGoods.filter(function (good) {
-          return good.favorite;
-        });
-      }
-
-      filteredGoods = filteredGoods.filter(function (good) {
-        return good.price >= filters.prices.min && good.price <= filters.prices.max;
-      });
-
-      if (filters.sorting.length > 0) {
-        sortGoods(filters.sorting[0], filteredGoods);
-      }
+      sortGoods(filters.sorting, filteredGoods);
 
       return filteredGoods;
     },
